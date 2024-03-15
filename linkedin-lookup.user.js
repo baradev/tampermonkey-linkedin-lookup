2908a4b2e3892739c929887dc8e72630cbe9aacf
Barbora Klusackova <barboraklusackova@gmail.com>
2024-03-15 21:31:23 +1300

    working version

--- linkedin-lookup.user.js
+++ linkedin-lookup.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         LinkedIn Lookup in Gmail with Trusted-Types Helper
+// @name         LinkedIn Lookup in Gmail
 // @namespace    http://tampermonkey.net
 // @version      0.6.6
-// @description  Search LinkedIn by recipient's full name or email address with Trusted-Types Helper
+// @description  Search LinkedIn by recipient's full name or email address
 // @author       Barbora Klusackova
 // @match        *://mail.google.com/*
 // @match        *://contacts.google.com/widget/hovercard/*
@@ -13,72 +13,8 @@
 // @icon         https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.5.1.min.js#sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=
-// @downloadURL https://update.greasyfork.org/scripts/433051/Trusted-Types%20Helper.user.js
-// @updateURL https://update.greasyfork.org/scripts/433051/Trusted-Types%20Helper.meta.js
 // ==/UserScript==
 
-// Trusted-Types Helper script starts here
-const overwrite_default = false // If a default policy already exists, it might be best not to overwrite it, but to try and set a custom policy and use it to manually generate trusted types. Try at your own risk
-const prefix = GM_info.script.name
-var passThroughFunc = function (string, sink) {
-  return string // Anything passing through this function will be returned without change
-}
-var TTPName = 'passthrough'
-var TTP_default,
-  TTP = {
-    createHTML: passThroughFunc,
-    createScript: passThroughFunc,
-    createScriptURL: passThroughFunc,
-  } // We can use TTP.createHTML for all our assignments even if we don't need or even have Trusted Types; this should make fallbacks and polyfills easy
-var needsTrustedHTML = false
-function doit() {
-  try {
-    if (
-      typeof window.isSecureContext !== 'undefined' &&
-      window.isSecureContext
-    ) {
-      if (window.trustedTypes && window.trustedTypes.createPolicy) {
-        needsTrustedHTML = true
-        if (trustedTypes.defaultPolicy) {
-          log('TT Default Policy exists')
-          if (overwrite_default)
-            TTP = window.trustedTypes.createPolicy('default', TTP)
-          else TTP = window.trustedTypes.createPolicy(TTPName, TTP) // Is the default policy permissive enough? If it already exists, best not to overwrite it
-          TTP_default = trustedTypes.defaultPolicy
-
-          log(
-            "Created custom passthrough policy, in case the default policy is too restrictive: Use Policy '" +
-              TTPName +
-              "' in var 'TTP':",
-            TTP
-          )
-        } else {
-          TTP_default = TTP = window.trustedTypes.createPolicy('default', TTP)
-        }
-        log('Trusted-Type Policies: TTP:', TTP, 'TTP_default:', TTP_default)
-      }
-    }
-  } catch (e) {
-    log(e)
-  }
-}
-
-function log(...args) {
-  if ('undefined' != typeof prefix && !!prefix) args = [prefix + ':', ...args]
-  if ('undefined' != typeof debugging && !!debugging)
-    args = [
-      ...args,
-      new Error().stack
-        .replace(/^\s*(Error|Stack trace):?\n/gi, '')
-        .replace(/^([^\n]*\n)/, '\n'),
-    ]
-  console.log(...args)
-}
-
-doit()
-// Trusted-Types Helper script ends here
-
-// Original LinkedIn Lookup in Gmail script starts here
 var $ = window.jQuery
 
 const css = `
